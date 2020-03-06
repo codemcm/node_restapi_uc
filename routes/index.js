5bc56b4db4d059e89fab6b31de2f5f44c058f552
codemcm <codemcm@hotmail.com>
2020-03-06 17:08:44 -0600

    route persona add

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+//PERSONA
+const persona = require("../models/persona");
+router.get("/v1/personas", persona.getPersona);
 
 module.exports = router;
