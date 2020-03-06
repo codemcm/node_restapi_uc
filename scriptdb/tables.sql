e50ced4e42a71b15593b7063a377c00d0aa489e4
codemcm <codemcm@hotmail.com>
2020-03-06 17:08:23 -0600

    script tables add

--- scriptdb/tables.sql
+++ scriptdb/tables.sql
@@ -1,1 +1,22 @@
---Database name: people
+--Database name: schedule
+
+
+-- Table: public.persona
+
+-- DROP TABLE public.persona;
+
+CREATE TABLE public.persona
+(
+    persona_id integer NOT NULL DEFAULT nextval('persona_persona_id_seq'::regclass),
+    nombre character varying(50) COLLATE pg_catalog."default",
+    apellidos character varying(50) COLLATE pg_catalog."default",
+    created_date date DEFAULT NOW(),
+    CONSTRAINT persona_pkey PRIMARY KEY (persona_id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.persona
+    OWNER to postgres;
