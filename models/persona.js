be8a4ef348be79992944ad549027e86664459269
codemcm <codemcm@hotmail.com>
2020-03-06 18:14:31 -0600

    model persona add, edit, delete,read ok

--- models/persona.js
+++ models/persona.js
@@ -1,13 +1,13 @@
 const dbCon = require("../conf/config");
 
-function getPersona(req, res, next) {
+function getPersonas(req, res, next) {
     dbCon.any("SELECT * FROM  persona")
         .then(function (data) {
             res
                 .json({
                     status: "successfull",
                     data: data,
-                    message: "Consulta exitosa"
+                    message: "Retrived personas"
                 })
         }).catch(function (err) {
             console.log(err)
@@ -15,11 +15,95 @@ function getPersona(req, res, next) {
                 status: "error",
                 data: "Sin registros",
                 message: err
-            })
+            });
         })
 
 }
 
+function getPersonaById(req, res, next) {
+    var personaId = parseInt(req.params.personaId);
+    dbCon.one('SELECT * FROM persona WHERE persona_id =$1', personaId)
+        .then(function (data) {
+            res.json({
+                status: "successfull",
+                data: data,
+                message: "persona"
+            });
+        })
+        .catch(function (error) {
+            res.json({
+                status: "error",
+                data: "Persona recuperada con exito",
+                message: error
+            })
+        })
+}
+
+function insertarPersona(req, res, next) {
+    dbCon.none('INSERT INTO PUBLIC.persona(nombre, apellidos) VALUES(${nombre}, ${apellidos})', req.body)
+        .then(function () {
+            res.json({
+                status: "successfull",
+                data: "Without data",
+                message: "Persona insertada correctamente"
+            })
+        })
+        .catch(function (err) {
+            console.log(err)
+            res.json({
+                status: "error",
+                data: "no data",
+                message: err
+            })
+        })
+}
+
+function editarPersona(req, res, next) {
+    console.log(req.body)
+    dbCon.none("UPDATE persona SET nombre=$1, apellidos=$2 WHERE persona_id = $3",
+        [req.body.nombre, req.body.apellidos, req.body.persona_id])
+        .then(function () {
+            res.json({
+                status: "successfull",
+                data: "Without data",
+                message: "Persona editada con exito"
+            })
+        })
+        .catch(function (error) {
+            console.log(error)
+            res.json({
+                status: "error",
+                data: "no data",
+                message: error
+            })
+        })
+}
+
+function eliminarPersona(req, res, next) {
+    var personaId = parseInt(req.params.personaId);
+    dbCon.any("DELETE FROM persona WHERE persona_id= $1", personaId)
+        .then(function () {
+            res.json({
+                status: "successful",
+                data: "Without data",
+                message: "Persona eliminada con exito"
+            })
+        }
+        )
+        .catch(function (error) {
+            console.log(error)
+            res.json({
+                status: "error",
+                data: "no data",
+                message: error
+            })
+        })
+}
+
 module.exports = {
-    getPersona: getPersona
+    getPersonas: getPersonas,
+    getPersona: getPersonaById,
+    insertarPersona: insertarPersona,
+    editarPersona: editarPersona,
+    eliminarPersona: eliminarPersona
 }
